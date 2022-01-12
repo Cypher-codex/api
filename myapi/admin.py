7b61abb0d45a993c8071c640c6b8a5739520bbc2
Cypher-codex <68659933+Cypher-codex@users.noreply.github.com>
2022-01-12 09:34:33 +0530

    first commit

---
+++ myapi/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Hero
+admin.site.register(Hero)
---
+++ myapi/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Hero(models.Model):
+    name = models.CharField(max_length=60)
+    password = models.CharField(max_length=60)
+    def __str__(self):
+        return self.name
